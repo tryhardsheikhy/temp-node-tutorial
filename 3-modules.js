5c8a7c45d3c4e933b7b7f8e8e69a53b3a3be7fb4
tryhardsheikhy <supersheikhy@gmail.com>
2024-08-28 09:59:33 +0600

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//Modules
+
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const chickenbaked = require('./6-alternative-flavour')
+
+
+// sayHi ('susan')
+// sayHi (names.john)
+// sayHi (names.peter)
+
+require('./7-mind-grenade');
