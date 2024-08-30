bcbce7b08f5565f7cfa9894181afdbfe88f1ce99
tryhardsheikhy <supersheikhy@gmail.com>
2024-08-30 16:29:35 +0600

    till promises setup demonstration

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-//  npm - global command, comes with node
-// npm --version
+const {readFile} = require('fs');
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const getText = (path) => {
+  return  new Promise (
+    (resolve, reject) => {
+      readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        else {
+          resolve(data)
+        }
+      })
+  
+    }
+  )
+}
 
-// global dependency - use it in any project
-// npm install -g <packagename>
-// sudo npm install -g <packageName> (mac)
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _= require('lodash')
-
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
+getText ('./content/first.txt')
+.then((result) => {console.log(result)})
+.catch((err) => {console.log(err)})
